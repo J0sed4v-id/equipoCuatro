ac4b5795ddcd157a23045c79ffff71e541631ec2
LuRaM1208 <luisa.llanten@correounivalle.edu.co>
2025-12-03 17:36:56 -0500

    correción de inventory

--- app/src/main/java/com/example/myapplication/ui/home/InventoryWidgetViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/InventoryWidgetViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.myapplication.data.ProductRepository
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import java.text.NumberFormat
